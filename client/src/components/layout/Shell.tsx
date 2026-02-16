5b13ee9b6eb6963a9d31dc60c6ec7cc922d96907
chetanngavali <chetanngavali@gmail.com>
2026-02-16 17:59:22 +0530

    Remove left and right padding for full-width layout

--- client/src/components/layout/Shell.tsx
+++ client/src/components/layout/Shell.tsx
@@ -8,7 +8,7 @@ export function Shell({ children }: { children: React.ReactNode }) {
       <MobileSidebar />
       <main className="flex-1 lg:pl-64">
         <ScrollArea className="h-screen">
-          <div className="container max-w-7xl mx-auto p-4 md:p-8 space-y-8 pb-20 pt-20 lg:pt-8">
+          <div className="space-y-8 pb-20 pt-20 lg:pt-8">
             {children}
           </div>
         </ScrollArea>
